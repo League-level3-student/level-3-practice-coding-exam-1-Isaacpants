881bde72269932f5fd3686d263da0f0ccdd7f1b3
Isaacpants <dave@boyermoving.com>
2020-05-19 10:12:24 -0700

    finished

--- src/practice/Finder.java
+++ src/practice/Finder.java
@@ -7,23 +7,30 @@ public class Finder {
 	public Object lastIndexOf(ArrayList<String> arrayList, String string) {
 		// TODO Auto-generated method stub
 		String lowerCase = string.toLowerCase();
-		
+		int count = -1;
+//		for (int i = 0; i < arrayList.size(); i++) {
+//			if(arrayList.get(i).toLowerCase().equals(lowerCase) && arrayList.get(i+1).toLowerCase().equals(lowerCase)) {
+//				return 2;
+//			}
+//				
+//			
+//			if(arrayList.get(i).toLowerCase().equals(lowerCase)) {
+//				
+//				
+//				return i;	
+//			}
+//			
+//			
+//			
+//			}
+//	
 		for (int i = 0; i < arrayList.size(); i++) {
-			if(arrayList.get(i).toLowerCase().equals(lowerCase) && arrayList.get(i+1).toLowerCase().equals(lowerCase)) {
-				return 2;
-			}
-				
-			
 			if(arrayList.get(i).toLowerCase().equals(lowerCase)) {
+				count = i;
 				
-				
-				return i;	
 			}
-			
-			
-			
-			}
-	
-return -1;
+		}
+		
+return count;
 	}
 }
